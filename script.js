8b0dc161fc6b7110fe7c79fac4bbb3289c9f63d4
Jimmy Wilson <james.l.wilson364@gmail.com>
2023-09-23 16:55:55 -0500

    saturday afternoon stuck on appending

--- script.js
+++ script.js
@@ -3,43 +3,82 @@
 // in the html.
 // get a handle on objects
 var scheduleContainer = $('#scheduleContainer');
+var currentDayGrab = $('#currentDay');
 var timeArray = $(["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]);
 var timeArrayInts = $([9,10,11,12,13,14,15,16,17]);
 var currentTime = dayjs().hour();
+var currentDay = dayjs().format('dddd MMMM DD YYYY');
+var currentTimeInt = parseInt(currentTime);
+console.log(currentDay);
 console.log(timeArray.length);
 console.log(timeArrayInts.length);
-console.log(currentTime);
+
 // loop function to create blocks for hours 9-5 need to color code each time block
+function scheduleForm() { 
+  $(currentDayGrab).text(currentDay);
+  $(scheduleContainer).append('<div id=timeArrayLine class="row text-block past"></div>');
+  for (i=0; i < timeArrayInts.length; i++) {
+    
+    var timeArrayLine = $('#timeArrayLine');
+    var userEntry = $('#userEntry');
+    var timeBlock = $('#timeBlock');
+    timeBlock = timeArrayInts[i];
+    console.log(timeBlock);
 
-for (i=0; i < timeArrayInts.length; i++) {
-  
-  $(scheduleContainer).append('<div id=timeArrayLine class="row time-block past"></div>');
-  var timeArrayLine = $('#timeArrayLine');
-  $(timeArrayLine).append('<div class="col-2 col-md-1 hour text-center py-3">'+  timeArray[i] + '</div>');
-  $(timeArrayLine).append('<textarea class="col-6 col-md-10 description" rows=3></textarea>');
-  $(timeArrayLine).append('<button class="btn saveBtn col-2 col-md-1" aria-label=save><i class="fas fa-save" aria-hidden=true></i></button>');
-  
-  if (currentTime > timeArrayInts[i]) {
+    
+    // if (timeArrayInts[i] > currentTimeInt) {
+    //   $(userEntry).attr('style', 'background-color:#77dd77');
+    // } else if (timeArrayInts[i] === currentTimeInt) {
+    //   $(userEntry).attr('style', 'background-color:#ff6961');
+    // } else {
+    //   $(userEntry).attr('style', 'background-color: #d3d3d3');
+    // }
+    
+    $(timeArrayLine).append('<div class="col-2 col-md-1 hour text-center py-4 border border-dark" id=timeBlock>'+  timeArray[i] + '</div>');
+    $(timeArrayLine).append('<textarea class="col-6 col-md-10 description border border-dark" rows=3 id=userEntry></textarea>');
+    $(timeArrayLine).append('<button id=actionButton class="btn saveBtn col-2 col-md-1 border border-dark" aria-label=save><i class="fas fa-save" aria-hidden=true></i></button>');
+    console.log(timeArray[i]);
     console.log(timeArrayInts[i]);
-    // $(scheduleContainer).append('<div id=timeArrayLine class="row time-block past"></div>');
-    
-  } else if (currentTime === timeArrayInts[i]) {
-    // $(scheduleContainer).append('<div id=timeArrayLine class="row time-block present"></div>');
-    
-  } else if (currentTime < timeArrayInts[i]){
-    // $(scheduleContainer).append('<div id=timeArrayLine class="row time-block future"></div>');
-    
   }
-  
+ 
+}
+scheduleForm();
 
-  console.log(timeArray[i]);
-  console.log(scheduleContainer);
-  console.log(timeArrayInts[i]);
+// function to color code the hours blocks on the form
+function categorizeHours() {
+  
 }
 
-// capture events in each timeblock in local storage on button click and store them in the block
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+// console.log(timeArray[0]);
+//     var timeArrayButton = $('#actionButton');
+//     var userScheduledEvent = ("");
+//     var userText = $('#userEntry');
+//     // console.log(timeArrayButton);
+
+// // capture events in each timeblock in local storage on button click and store them in the block
+// $(function scheduledEvent() {
+//     timeArrayLine.addEventListener("click", function () {
+//       userScheduledEvent = userText.value;
+//       localStorage.setItem("9AM Event", userScheduledEvent);
+//       console.log(userScheduledEvent);
+//     });
+//   });
+
+//scheduledEvent();
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+// TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -57,17 +96,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
