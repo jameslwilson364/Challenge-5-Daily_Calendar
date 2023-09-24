bc47b227d5d34bd805c54410424995f03d06fe75
Jimmy Wilson <james.l.wilson364@gmail.com>
2023-09-23 20:27:26 -0500

    making progress

--- script.js
+++ script.js
@@ -5,25 +5,46 @@
 var scheduleContainer = $('#scheduleContainer');
 var currentDayGrab = $('#currentDay');
 var timeArray = $(["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]);
+// var userEntry = $('<div>');
 var timeArrayInts = $([9,10,11,12,13,14,15,16,17]);
 var currentTime = dayjs().hour();
 var currentDay = dayjs().format('dddd MMMM DD YYYY');
 var currentTimeInt = parseInt(currentTime);
 console.log(currentDay);
+console.log(currentTime);
 console.log(timeArray.length);
 console.log(timeArrayInts.length);
 
 // loop function to create blocks for hours 9-5 need to color code each time block
 function scheduleForm() { 
   $(currentDayGrab).text(currentDay);
-  $(scheduleContainer).append('<div id=timeArrayLine class="row text-block past"></div>');
+  $(scheduleContainer).append('<div id=timeArrayLine ></div>');
   for (i=0; i < timeArrayInts.length; i++) {
-    
-    var timeArrayLine = $('#timeArrayLine');
-    var userEntry = $('#userEntry');
-    var timeBlock = $('#timeBlock');
-    timeBlock = timeArrayInts[i];
-    console.log(timeBlock);
+      var timeArrayLine = $('#timeArrayLine');
+      var userEntry = $('#userEntry');
+      var timeArrayLineTest = $('<div id=timeArrayLineTest class="row text-block past">');
+      console.log(currentTime);
+      timeArrayLine.append(timeArrayLineTest);
+      $(timeArrayLineTest).append('<div class="col-2 col-md-1 hour text-center py-4 border border-dark" id=timeBlock>'+  timeArray[i] + '</div>');
+      $(timeArrayLineTest).append('<textarea class="col-6 col-md-10 description border border-dark" rows=3 id=userEntry></textarea>');
+      $(timeArrayLineTest).append('<button id=actionButton class="btn saveBtn col-2 col-md-1 border border-dark" aria-label=save><i class="fas fa-save" aria-hidden=true></i></button>');
+      var timeCheck = parseInt(timeArrayInts[i]);
+      for (j=0; j < timeArrayLineTest.length; j++) {
+        console.log(timeArrayLineTest[j]);
+        console.log(timeCheck);
+        if (timeCheck < currentTimeInt) {
+          //$(timeArrayLineTest.children[3]).attr('style', 'background-color:#77dd77');
+          $(timeArrayLineTest).attr('style', 'background-color:#77dd77');
+        } else if (timeArrayInts[i] === currentTimeInt) {
+          $(userEntry[i]).attr('style', 'background-color:#ff6961');
+        } else {
+          $(userEntry[i]).attr('style', 'background-color: #d3d3d3');
+        }
+      }
+    // var userEntry = $('<div class="col-2 col-md-1 hour text-center py-4 border border-dark" id=timeBlock>'+  timeArray[i] + '</div>');
+    // var timeBlock = $('<#timeBlock>');
+    // timeBlock = timeArrayInts[i];
+    // console.log(timeBlock);
 
     
     // if (timeArrayInts[i] > currentTimeInt) {
@@ -34,9 +55,10 @@ function scheduleForm() {
     //   $(userEntry).attr('style', 'background-color: #d3d3d3');
     // }
     
-    $(timeArrayLine).append('<div class="col-2 col-md-1 hour text-center py-4 border border-dark" id=timeBlock>'+  timeArray[i] + '</div>');
-    $(timeArrayLine).append('<textarea class="col-6 col-md-10 description border border-dark" rows=3 id=userEntry></textarea>');
-    $(timeArrayLine).append('<button id=actionButton class="btn saveBtn col-2 col-md-1 border border-dark" aria-label=save><i class="fas fa-save" aria-hidden=true></i></button>');
+   
+    // $(timeArrayLine).append('<div class="col-2 col-md-1 hour text-center py-4 border border-dark" id=timeBlock>'+  timeArray[i] + '</div>');
+    // $(timeArrayLine).append('<textarea class="col-6 col-md-10 description border border-dark" rows=3 id=userEntry></textarea>');
+    // $(timeArrayLine).append('<button id=actionButton class="btn saveBtn col-2 col-md-1 border border-dark" aria-label=save><i class="fas fa-save" aria-hidden=true></i></button>');
     console.log(timeArray[i]);
     console.log(timeArrayInts[i]);
   }
